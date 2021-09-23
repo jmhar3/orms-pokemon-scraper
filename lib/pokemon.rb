012e9923b063844c5cf90f34cad9c69455665792
jmhar <jmhar@protonmail.com>
2021-09-23 17:07:30 +1000

    pokemons

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,24 @@
 class Pokemon
+    attr_accessor :name, :type
+    attr_reader :id, :db
+
+    def initialize name:, type:, id: nil, db:
+        @name = name
+        @type = type
+        @id = id
+        @db = db
+    end
+
+    def self.save name, type, db
+        sql = "INSERT INTO pokemon (name, type)
+            VALUES (?, ?);"
+        db.execute(sql, name, type)
+        @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+    end
+
+    def self.find id, db
+        sql = "SELECT * FROM pokemon WHERE id = ?;"
+        poke_data = db.execute(sql, id)[0]
+        Pokemon.new(id: poke_data[0], name: poke_data[1], type: poke_data[2], db: db)
+    end
 end
